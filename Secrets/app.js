b0069903570e43778bd9a9ef0c4b0c1a2b566706
Kuhsun <ek781119@diamondonline.come.tw>
2023-04-26 19:27:00 +0800

    Level 4 - salt it with bcrypt

--- Secrets/app.js
+++ Secrets/app.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -33,36 +35,40 @@ app.get("/register", function (req, res) {
 })
 
 app.post("/register", function (req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-    newUser
-        .save()
-        .then(function () {
-            res.render("secrets");
-        })
-        .catch(function (err) {
-            res.send(err);
-        });
 
+    bcrypt
+        .hash(req.body.password, saltRounds, function (err, hash) {
+            const newUser = new User({email: req.body.username, password: hash});
+            newUser
+                .save()
+                .then(function () {
+                    res.render("secrets");
+                })
+                .catch(function (err) {
+                    res.send(err);
+                });
+        });
 })
 
 app.post("/login", function (req, res) {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User
         .findOne({email: username})
-        .then(function (result) {
-            if (result) {
-                if (result.password === password) {
-                    res.render("secrets");
-                } else {
-                    res.send("Wrong login information.");
-                }
+        .then(function (foundUser) {
+            if (foundUser) {
+                bcrypt
+                    .compare(password, foundUser.password)
+                    .then(function (result) {
+                        if (result === true) {
+                            res.render("secrets");
+                        } else {
+                            res.send("Wrong information. Please try to login again.")
+                        }
+                    });
             } else {
-                res.send("Wrong login information.");
+                res.send("Wrong information. Please try to login again.")
             }
         })
         .catch(function (err) {
